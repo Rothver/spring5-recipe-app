26c0f54ab4111dc83240865ae071b0f7b71425f2
Arturo <arturo.norico.3@gmail.com>
2022-07-14 18:03:45 -0400

    Added dataloading and refactored code

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -27,7 +27,7 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
-    
+
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
@@ -38,10 +38,8 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
         joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
-    
+    private Set<Category> categories = new HashSet<>();
 
-    
     public Long getId() {
         return id;
     }
@@ -120,6 +118,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Difficulty getDifficulty() {
@@ -136,13 +149,5 @@ public class Recipe {
 
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
     }
 }
